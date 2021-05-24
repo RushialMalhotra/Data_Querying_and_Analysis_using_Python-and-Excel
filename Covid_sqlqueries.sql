8abe8033c5d3bd1eab4d2b3154afa50a46188988
Rushial Malhotra <55552028+RushialMalhotra@users.noreply.github.com>
2021-05-24 13:51:46 +0530

    Update Covid_sqlqueries.sql
    The following is the set of SQL queries used for the analysis of Covid data across the World. 
    Data Source : https://ourworldindata.org/covid-deaths. 
    The following queries were executed on SQL-Server to develop insights on Covid deaths, Covid cases and it's affect on a global level. 
    db.name : Portfolio_Project. Tables used : covid_deaths & covid_vaccination

--- Covid_sqlqueries.sql
+++ Covid_sqlqueries.sql
@@ -1,4 +1,10 @@
---Viewing both tables before querying
+--The following is the set of SQL queries used for the analysis of Covid data across the World. 
+--Data Source : https://ourworldindata.org/covid-deaths. 
+--db.name : Portfolio_Project. Tables used : covid_deaths & covid_vaccination
+--The following queries were executed on Sql-Server to develop insights on covid deaths, covid cases and it's affect on a global level.
+
+
+--1. Viewing both tables before querying
 
 Select *
 From Portfolio_Project..covid_deaths
@@ -8,7 +14,7 @@ Select *
 From Portfolio_Project..covid_vaccination
 order by 1,2
 
---Select data which we are going to use
+--2. Select data which we are going to use
 
 Select location, date, total_cases,new_cases, total_deaths, population
 From Portfolio_Project..covid_deaths
@@ -16,7 +22,7 @@ where continent is not null
 order by 1,2
 
 
--- Looking at Total Cases vs Total Deaths
+-- 3. Looking at Total Cases vs Total Deaths
 -- Shows likelihood of dying if you contract covid in your country
 
 Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
@@ -25,7 +31,7 @@ where location = 'india'
 order by 1,2
 
 
--- Looking at Total Cases vs Population
+-- 4. Looking at Total Cases vs Population
 
 Select location, date, population, total_cases, (total_cases/population)*100 as InfectedPercentage
 From Portfolio_Project..covid_deaths
@@ -33,7 +39,7 @@ where location = 'india'
 order by 1,2
 
 
---Looking at Countries with Highest Infection rate compared to the Population
+--5. Looking at Countries with Highest Infection rate compared to the Population
 
 Select location, population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/population))*100 as PercentPerPopulation
 From Portfolio_Project..covid_deaths
@@ -41,7 +47,7 @@ Group by location, population
 Order by PercentPerPopulation desc
 
 
---Looking at Countries with Highest Death Count per Population
+--6. Looking at Countries with Highest Death Count per Population
 
 Select location, MAX(cast(total_deaths as int)) as TotalDeathCount
 From Portfolio_Project..covid_deaths
@@ -50,7 +56,7 @@ Group by location
 Order by TotalDeathCount desc
 
 
---Looking at Death Count per Continent
+--7. Looking at Death Count per Continent
 
 Select continent, MAX(cast(total_deaths as int)) as TotalDeathCount
 From Portfolio_Project..covid_deaths
@@ -59,7 +65,7 @@ Group by continent
 Order by TotalDeathCount desc
 
 
---Global Numbers on basis of time(Date of contraction)
+--8. Global Numbers on basis of time(Date of contraction)
 
 Select date, SUM(new_cases) as Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int)) / SUM(new_cases)*100 as DeathPercentage
 From Portfolio_Project..covid_deaths
@@ -68,7 +74,7 @@ Group by date
 Order by 1,2
 
 
---Total infection and deaths on the global level
+--9. Total infection and deaths on the global level
 
 Select SUM(new_cases) as Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int)) / SUM(new_cases)*100 as DeathPercentage
 From Portfolio_Project..covid_deaths
@@ -76,7 +82,7 @@ where continent is not null
 Order by 1,2
 
 
---Looking at TotalPopulation vs Vaccinations
+--10. Looking at TotalPopulation vs Vaccinations
 
 Select dea.continent, dea.location,dea.date, dea.population, vacc.new_vaccinations, 
 SUM(Convert(int,vacc.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.date) as RollingPeopleVaccinated
@@ -88,7 +94,7 @@ where dea.continent is not null
 Order by 2,3
 
 
---Using CTE
+--11. Using CTE
 
 With PopvsVacc (Continent, Location, Date, population, New_vaccinations, RollingPeopleVaccinated)
 as(
@@ -105,7 +111,7 @@ Select *, (RollingPeopleVaccinated/population)*100
 From PopvsVacc
 
 
---Using Temp Table
+--12. Using Temp Table
 
 Drop table if exists #PercentPopulationVaccinated
 Create table #PercentPopulationVaccinated
@@ -129,7 +135,7 @@ Select *, (RollingPeopleVaccinated/population)*100
 From #PercentPopulationVaccinated
 
 
---Creating view to store data for future visualizations
+--13. Creating view to store data for future visualizations
 
 Create View PercentagePopulationVaccinated As
 (
